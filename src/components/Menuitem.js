75eb19c4d5ba89f963f9abec0b3e422226abecb5
RHARRABT <rharrabt@MacBook-Pro-de-RHARRABT.local>
2020-11-10 22:06:40 +0100

    versiona

--- src/components/Menuitem.js
+++ src/components/Menuitem.js
@@ -11,13 +11,23 @@ const MenuItem = ({ data, products, setProducts }) => {
       }}
     >
       <div>
-        <p>{data.title}</p>
-        <p>{data.description && data.description.slice(0, 60) + "..."}</p>
+        <div className="title">
+          <p>{data.title}</p>
+        </div>
+        <div className="description">
+          <p>{data.description && data.description.slice(0, 60) + "..."}</p>
+        </div>
         <p>{data.price}</p>
       </div>
-      {data.picture && (
-        <img style={{ height: 100 }} src={data.picture} alt={data.title} />
-      )}
+      <div className="image">
+        {data.picture && (
+          <img
+            style={{ height: 130, width: 130 }}
+            src={data.picture}
+            alt={data.title}
+          />
+        )}
+      </div>
     </div>
   );
 };
